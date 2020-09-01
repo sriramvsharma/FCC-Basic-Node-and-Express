edaeb056796da28f69010b016f110717e4f3f5ce
Glitch (eastern-earthy-rake) <none>
2020-09-01 03:16:21 +0000

    🚓📮 Updated with Glitch

---
+++ myApp.js
@@ -0,0 +1,141 @@
+
+// var express = require('express');
+// var app = express();
+
+// --> 7)  Mount the Logger middleware here
+
+
+
+// --> 11)  Mount the body-parser middleware  here
+
+var bodyParser = require('body-parser')
+var express = require('express');
+var app = express();
+
+
+/** 1) Meet the node console. */
+console.log("Hello World")
+
+/** 2) A first working Express Server */
+app.get('/', function(req, res){
+  res.send('Hello Express');
+})
+
+/** 3) Serve an HTML file */
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/views/index.html");
+})
+
+
+// /** 4) Serve static assets  */
+app.use(express.static(__dirname + '/public'))
+
+
+/** 5) serve JSON on a specific route */
+//Let's create a simple API by creating a route that responds with JSON at the path /json. 
+//You can do it as usual, with the app.get() method. 
+//Inside the route handler, use the method res.json(), passing in an object as an argument. 
+//This method closes the request-response loop, returning the data. 
+//Behind the scenes, it converts a valid JavaScript object into a string, then sets the appropriate headers
+//to tell your browser that you are serving JSON, and sends the data back. 
+//A valid object has the usual structure {key: data}. 
+//data can be a number, a string, a nested object or an array. 
+//data can also be a variable or the result of a function call, 
+//in which case it will be evaluated before being converted into a string.
+
+app.get("/json", function(req, res){
+  res.json({"message": "Hello json"})
+})
+
+// var message  = {"message": "Hello Json"}
+
+// /** 6) Use the .env file to configure the app */
+
+app.get('/json', (req, res)=> {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+  res.json ({"message": "HELLO JSON"})
+  } else {
+    res.json (message)}
+  })
+
+// app.get('/json', (req, res)=> {
+// if (process.env.MESSAGE_STYLE === "uppercase") {
+// res.json ({"message": "HELLO JSON"})
+// }
+// res.json (message)
+// })
+
+
+
+var message = {"message":"Hello json"}
+app.get("/json", (req, res) => {
+if (process.env.MESSAGE_STYLE === "uppercase") {
+res.json ({"message": "HELLO JSON"})
+}
+res.json (message)
+})
+
+
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+
+app.use((req, res, next)=> {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+  next();
+})
+
+
+/** 8) Chaining middleware. A Time server */
+
+app.get('/now', function(req, res, next) {
+  req.time = {time: new Date().toString()}  // Hypothetical synchronous operation
+  next();
+}, function(req, res) {
+  res.send(req.time);
+})
+
+/** 9)  Get input from client - Route parameters */
+
+app.get("/:word/echo", function(req, res){
+  
+  var message = {echo: req.params.word}
+  res.send(message)
+})
+
+/** 10) Get input from client - Query parameters */
+// /name?first=<firstname>&last=<lastname>
+
+app.get('/name', function(req, res){
+  var {first: firstName, last: lastName} = req.query
+  
+  res.json({name: `${firstName} ${lastName}`})
+})
+
+  
+/** 11) Get ready for POST Requests - the `body-parser` */
+// place it before all the routes !
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+
+/** 12) Get data form POST  */
+
+app.post("/name", function(req, res) {
+  // Handle the data in the request
+  var string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
+});
+
+
+
+// This would be part of the basic setup of an Express app
+// but to allow FCC to run tests, the server is already active
+/** app.listen(process.env.PORT || 3000 ); */
+
+//---------- DO NOT EDIT BELOW THIS LINE --------------------
+
+ module.exports = app;
